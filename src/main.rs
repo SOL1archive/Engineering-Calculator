d6ee7a61bea815e77156c57fee52adef7819f8b2
Subin Park <75925053+SOL1archive@users.noreply.github.com>
2024-04-26 11:08:59 +0000

    update:test code

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,18 @@
-use std::collections::LinkedList;
-
 pub mod term;
 pub mod formula;
+
+use crate::term::{OperatorType, TermType, Term, Formula};
 
 fn main() {
     
 }
 
 #[test]
-fn term_copy_test() {
-    let term1: term::Polymomial = term::Polymomial::new(1., 1.);
-    let term2: term::Polymomial = term1;
-
-    assert_eq!(term2, term1);
-}
-
-#[test]
 fn evaluation_test() {
-    let linear = term::Polymomial::new(2., 1.);
-    let x2 = term::Polymomial::new(1., 2.);
-    let log10 = term::Log::new(1., 1., 10.);
-    let exp2 = term::Exp::new(1., 2.);
+    let linear = Term::new(OperatorType::Plus, TermType::Polymomial, 2., 0.);
+    let x2 = Term::new(OperatorType::Plus, TermType::Polymomial, 1., 2.);
+    let log10 = Term::new(OperatorType::Plus, TermType::Log { base: 10.}, 1., 1.);
+    let exp2 = Term::new(OperatorType::Plus, TermType::Exp { base: 2. }, 1., 1.);
 
     assert_eq!(linear.evaluate(1.), 2.);
     assert_eq!(x2.evaluate(2.), 4.);
@@ -30,17 +22,5 @@ fn evaluation_test() {
 
 #[test]
 fn formula_evaluate() {
-    let mut formula1 = formula::Formula::new();
-    let mut list: LinkedList<term::Term> = LinkedList::new();
-    formula1.push_back(term::Polymomial::new(2., 0.));
-    formula1.push_back(term::Polymomial::new(1., 2.));
-    formula1.push_back(term::Exp::new(1., std::f64::consts::E));
     
-    list.push_back(term::Polymomial::new(1., 2.));
-    list.push_back(term::Polymomial::new(2., 0.));
-    list.push_back(term::Exp::new(1., std::f64::consts::E));
-    let mut formula2 = formula::Formula::new_linked_list(&mut list);
-
-    assert_eq!(formula1.evaluate(0.), 1.);
-    assert_eq!(formula2.evaluate(0.), 1.);
 }
