c889e12692d9e5781be2ab75c8fbb481377381fe
Subin Park <75925053+SOL1archive@users.noreply.github.com>
2024-05-02 23:49:18 +0000

    update:lexer

--- src/parser.rs
+++ src/parser.rs
@@ -19,11 +19,15 @@ pub enum Token {
     Exp(String),
 }
 
-pub fn parse(text: String) -> LinkedList<Token> {
+pub fn lexer(text: String) -> LinkedList<Token> {
     let mut vec :LinkedList<Token> = LinkedList::new();
+    let mut start_pos: usize = text.len();
     for (i, ch) in text.chars().enumerate() {
         if ch.is_whitespace() {
-            continue;
+            if start_pos != text.len() {
+                vec.push_back(Token::Ident(String::from(&text[start_pos..i])));
+                start_pos = text.len();
+            }
         } else if ch == '+' {
             vec.push_back(Token::Plus(String::from('+')));
         } else if ch == '-' {
@@ -36,6 +40,10 @@ pub fn parse(text: String) -> LinkedList<Token> {
             vec.push_back(Token::Lparen(String::from('(')));
         } else if ch == ')' {
             vec.push_back(Token::Rparen(String::from(')')));
+        } else if ch.is_alphanumeric() {
+            if start_pos == text.len() {
+                start_pos = i;
+            }
         }
     }
 
